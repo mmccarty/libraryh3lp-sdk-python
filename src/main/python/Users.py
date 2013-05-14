ca4ba92f487b6c26f055c1fb3d0f51bdf02621b8
Mike McCarty <mike@nubgames.com>
2013-05-14 12:39:52 -0400

    assignments working

--- src/main/python/Users.py
+++ src/main/python/Users.py
@@ -25,3 +25,20 @@ class Users (Resource):
     def deleteUser(self, uuid):
         r = self.delete(url = "/%s" % uuid)
         return r.json(), r.status_code
+
+    def listAssignments(self, uuid):
+        r = self.get(url = "/%s/assignments" % uuid)
+        return r.json(), r.status_code
+
+    def createAssignment(self, uuid, data):
+        r = self.post(url = "/%s/assignments" % uuid, data = data)
+        return r.json(), r.status_code
+
+    def saveAssignment(self, uuid, queueUuid, data):
+        r = self.put(url = "/%s/assignments/%s" % (uuid, queueUuid), data = data)
+        return r.json(), r.status_code
+
+    def destroyAssignment(self, uuid, queueUuid):
+        r = self.delete(url = "/%s/assignments/%s" % (uuid, queueUuid))
+        return r.json(), r.status_code
+        
--- test/python/TestUsers.py
+++ test/python/TestUsers.py
@@ -34,6 +34,9 @@ class TestUsers(Lh3Test):
         self.assertEqual(results[0]['type'], 'folder')
 
     def test_100createUser(self):
+        results, status = self.users.deleteUser("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88")
+        self.assertEqual(status, requests.codes.ok)
+        
         data = {'name' : 'unittest&',
                 'password' : 'asdf5!',
                 'type'     : 'user',
@@ -69,8 +72,39 @@ class TestUsers(Lh3Test):
 
         self.assertEqual(status, requests.codes.ok)
         self.assertEqual(results[0]['email'], data['email'])
+
+    def test_102listAssignments(self):
+        results, status = self.users.listAssignments("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88")
+        self.assertEqual(status, requests.codes.ok)
+        self.assertTrue(len(results) == 0)
+
+        results, status = self.users.listAssignments("dd09a03e-d527-4783-b7dd-6fdd8aecef6e")
+        self.assertEqual(status, requests.codes.ok)
+        self.assertTrue(len(results) > 0)
         
-    def test_102deleteUser(self):
+    def test_103createAssignment(self):
+        results, status = self.users.listAssignments("dd09a03e-d527-4783-b7dd-6fdd8aecef6e")
+        self.assertEqual(status, requests.codes.ok)
+
+        data = {'queue-uuid' : results[0]['queueUuid']}
+
+        results, status = self.users.createAssignment("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88", data = data)
+        self.assertEqual(status, requests.codes.ok)
+
+    def test_104saveAssignment(self):
+        results, _      = self.users.listAssignments("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88")
+        results, status = self.users.saveAssignment("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88",
+                                                    results[0]['queueUuid'],
+                                                    data = {'enabled' : False})
+        self.assertEqual(status, requests.codes.ok)
+        self.assertFalse(results[0]['enabled'])
+
+    def test_105destroyAssignment(self):
+        results, _      = self.users.listAssignments("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88")
+        results, status = self.users.destroyAssignment("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88", results[0]['queueUuid'])
+        self.assertEqual(status, requests.codes.ok)
+        
+    def test_110deleteUser(self):
         results, status = self.users.deleteUser("deccfef7-0cdc-4ed1-a827-b3dbd4cc3d88")
         self.assertEqual(status, requests.codes.ok)
         self.assertTrue(results['success'])
