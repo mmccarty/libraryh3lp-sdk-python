5c0aed1a88e48cf8deb0ec899ebbbba80426958f
Mike McCarty <mike@nubgames.com>
2013-06-06 13:00:41 -0400

    modified chats api to use ids rather than uuids

--- resources/Chats.py
+++ resources/Chats.py
@@ -14,31 +14,31 @@ class Chats (Resource):
         response = self.httpRequest('GET', url = "/%s" % dt.strftime('%Y/%m/%d'), params = params)
         return (response.json(), response.status_code) if format == 'json' else (response, response.status_code)
 
-    def listMonth(self, year, month, to = None): 
+    def listMonth(self, year, month, to = None):
         dt = datetime(year, month, 1)
         if to:
             params['to'] = to
         response = self.httpRequest('GET', url = "/%s" % dt.strftime('%Y/%m'))
         return response.json(), response.status_code
 
-    def listYear(self, year, to = None): 
+    def listYear(self, year, to = None):
         if to:
             params['to'] = to
         response = self.httpRequest('GET', url = "/%s" % year)
         return response.json(), response.status_code
 
-    def anonymizeChats(self, uuids):
-        response = self.httpRequest('POST', url = "/anonymize-conversations", params = {'uuids' : ','.join(uuids)})
+    def anonymizeChats(self, ids):
+        response = self.httpRequest('POST', url = "/anonymize-conversations", params = {'ids' : ','.join(map(str, ids))})
         return response.json(), response.status_code
 
-    def deleteChats(self, uuids):
-        response = self.httpRequest('POST', url = "/delete-conversations", params = {'uuids' : ','.join(uuids)})
+    def deleteChats(self, ids):
+        response = self.httpRequest('POST', url = "/delete-conversations", params = {'ids' : ','.join(map(str, ids))})
         return response.json(), response.status_code
 
-    def deleteTranscripts(self, uuids):
-        response = self.httpRequest('POST', url = "/delete-transcripts", params = {'uuids' : ','.join(uuids)})
+    def deleteTranscripts(self, ids):
+        response = self.httpRequest('POST', url = "/delete-transcripts", params = {'ids' : ','.join(map(str, ids))})
         return response.json(), response.status_code
 
-    def archiveConversations(self, uuids):
-        response = self.httpRequest('GET', url = "/archive", params = {'uuids' : ','.join(uuids)})
+    def archiveConversations(self, ids):
+        response = self.httpRequest('GET', url = "/archive", params = {'ids' : ','.join(map(str, ids))})
         return response.content, response.status_code
