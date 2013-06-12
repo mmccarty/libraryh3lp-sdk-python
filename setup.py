ef1999f584af8d3d21bc4f0e0491e8f33a072edb
Eric Sessoms <eric@nubgames.com>
2013-06-12 11:56:47 -0400

    added setup script and renamed resources to libraryh3lp
    "resource" was a bit generic, and too close to the built-in "resource".
    "libraryh3lp" is more useful for 3rd-party installs.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = "libraryh3lp-sdk",
+	version = "0.1.0",
+	description = "Convenience wrappers for the LibraryH3lp REST API.",
+	author = "Mike McCarty",
+	author_email = "mike@nubgames.com",
+	url = "https://github.com/nubgames/libraryh3lp-sdk-python",
+	packages = ["libraryh3lp"]
+	)
